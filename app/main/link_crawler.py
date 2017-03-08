3e60a281d0d12cb84988159d1d848dbe9634d947
Cao Jian <pycaojian@163.com>
2017-03-08 18:22:04 +0800

    multiprocessing the crawler

--- app/main/link_crawler.py
+++ app/main/link_crawler.py
@@ -2,17 +2,26 @@
 import re
 import csv
 import threading
+import chardet
+import multiprocessing
 from mongo_cache import MongoCache
 from download import Download
+from mongo_queue import Mongo_Queue
 
 
 def link_crawler(seed_url,link_regex_large,link_regex_small,max_depth=2,max_threads=5):
 	'Crawl from the given seed URL following links matchedly by link_regex'
-	crawl_queue=[seed_url]
-	seen={seed_url:0}
+	print 'seed_ur',seed_url
+	#crawl_queue=[seed_url]
+	crawl_queue=Mongo_Queue()
+	#seen={seed_url:0}#no need this seen for Mongo_Queue will take care of duplicate url
+	#crawl_queue=Mongo_Queue.push(seed_url)
+	crawl_queue.push(seed_url)
+	depth=(crawl_queue.get_item(seed_url))['depth']
+	print 'seedurldepth:',depth
 	cache=MongoCache()
 	D=Download(cache=cache)
-	result_links=set()
+	#result_links=set()
 	csvFile=open('D:/Work/Projects/realestate/app/static/163_money.csv','wb')
 	writer=csv.writer(csvFile)
 
@@ -21,11 +30,14 @@ def link_crawler(seed_url,link_regex_large,link_regex_small,max_depth=2,max_thre
 		while True:
 			try:
 				url=crawl_queue.pop()
-			except IndexError:
+			except KeyError:
+				#no url in crawl_queue
 				break
 			else:
-				depth=seen[url]
-				if depth!=max_depth:
+				depth=(crawl_queue.get_item(url))['depth']
+				'depth=128,129'
+				#print depth
+				if depth<=max_depth:
 					html=D(url)
 					links=re.findall(link_regex_large,html)
 
@@ -35,10 +47,12 @@ def link_crawler(seed_url,link_regex_large,link_regex_small,max_depth=2,max_thre
 							#writer.writerow((link,''))
 							print link
 						else:
-							#if not re.match(link_regex_exclude,link):
-							if link not in seen:
-								seen[link]=depth+1
-								crawl_queue.append(link)
+							crawl_queue.push(link,depth+1)
+							#encoding=chardet.detect(link)
+							#link=link.decode(encoding).encode('utf-8')
+							#crawl_queue.push(link,depth+1)
+							#seen[link]=depth+1
+				crawl_queue.complete(url)
 								
 	threads=[]
 	while crawl_queue or threads:
@@ -54,4 +68,19 @@ def link_crawler(seed_url,link_regex_large,link_regex_small,max_depth=2,max_thre
 			if not thread.is_alive():
 				#remove the stopped threads
 				threads.remove(thread)
+
 	csvFile.close()
+
+def process_link_crawler(*args,**kwargs):
+	#num_cpus=multiprocessing.cpu_count()
+	num_cpus=2
+	print 'Starting {} processes'.format(num_cpus)
+	processes=[]
+	for i in range(num_cpus):
+		p=multiprocessing.Process(target=link_crawler,args=args,kwargs=kwargs)
+		p.start()
+		processes.append(p)
+	#wait for processes to complete
+	for p in processes:
+		#Block the calling thread until the process whose join() m3ethod is called terminates or until the optional timeout occurs.
+		p.join()
--- app/main/main.py
+++ app/main/main.py
@@ -4,19 +4,20 @@ import re
 import download
 import link_crawler
 import csv
+import multiprocessing
 
-start=datetime.datetime.now()
-date=start.strftime('%y/%m%d')
-url='http://money.163.com/'
-link_regex_large=re.compile('<a href="(http://money.163.com/.*?)"')
-link_regex_small=re.compile('http://money.163.com/'+date+'.*\.html$')
+if __name__=='__main__':
+	#multiprocessing.freeze_support()
+	start=datetime.datetime.now()
+	date=start.strftime('%y/%m%d')
+	url='http://money.163.com/'
+	link_regex_large=re.compile('<a href="(http://money.163.com/.*?)"')
+	link_regex_small=re.compile('http://money.163.com/'+date+'.*\.html$')
 
-
-link_crawler.link_crawler(url,link_regex_large,link_regex_small)
-
-end=datetime.datetime.now()
-process_time=(end-start).total_seconds()
-print process_time
+	link_crawler.process_link_crawler(url,link_regex_large,link_regex_small,max_depth=2,max_threads=5)
+	end=datetime.datetime.now()
+	process_time=(end-start).total_seconds()
+	print process_time
 '''
 csvFile=open('D:/Work/Projects/realestate/app/static/163_money.csv','wb')
 try:
